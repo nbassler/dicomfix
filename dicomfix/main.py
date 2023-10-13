005334ae936059792087ca2fd25882ba4ab029be
Niels Bassler <bassler@clin.au.dk>
2023-10-13 21:36:08 +0200

    Add more requested features.

--- dicomfix/main.py
+++ dicomfix/main.py
@@ -34,53 +34,73 @@ def print_comparison(layer, scale_factor, original_weights, modified_weights, nu
 
 def rescale(parsed_args, dicom_data, new_dicom_data):
 
-    ion_beam_sequence = new_dicom_data.IonBeamSequence[0]
-    ion_control_point_sequence = new_dicom_data.IonBeamSequence[0].IonControlPointSequence
-
     scale_factors = []
-
     if parsed_args.weights:
         scale_factors = read_weights_from_csv(parsed_args.weights)
 
     plan_rescale_ratio = 1.0  # Initialize to 1 so it doesn't affect multiplication if not set
+
     if parsed_args.plan_dose and parsed_args.rescale_dose:
         plan_rescale_ratio = parsed_args.rescale_dose / parsed_args.plan_dose
 
-    total_original_cumulative_weight = 0.0
-    total_new_cumulative_weight = 0.0
+    if parsed_args.rescale_factor:
+        plan_rescale_ratio = parsed_args.rescale_factor
 
-    for i in range(0, len(ion_control_point_sequence)):
-        weights = ion_control_point_sequence[i].ScanSpotMetersetWeights
-        original_cumulative_weight = new_dicom_data.IonBeamSequence[0].IonControlPointSequence[i].CumulativeMetersetWeight
-        total_original_cumulative_weight += original_cumulative_weight  # Add to total original cumulative weight
+    for j,ion_beam in enumerate(new_dicom_data.IonBeamSequence):  # loop over fields
+        logging.info(f"Rescaling field {j+1}")
+        ion_control_point_sequence = ion_beam.IonControlPointSequence
 
-        # Use scale factor if available, otherwise use 1
-        scale_factor = scale_factors[i] if scale_factors else 1
+        total_original_cumulative_weight = 0.0
+        total_new_cumulative_weight = 0.0
 
-        # Modify the weights with both scale_factor and plan_rescale_ratio
-        new_weights = [w * scale_factor * plan_rescale_ratio for w in weights]
-        logging.info(f"length of weights {len(weights)}")
-        for w in weights:
-            logging.debug(f'wt: {w}')
-        new_cumulative_weight = original_cumulative_weight * scale_factor * plan_rescale_ratio
-        #sum(new_weights)  # Calculate the new cumulative weight
-        total_new_cumulative_weight += new_cumulative_weight  # Add to total new cumulative weight
+        for i,ion_control_point in enumerate(ion_beam.IonControlPointSequence):  # loop over energy layers
 
-        new_dicom_data.IonBeamSequence[0].IonControlPointSequence[i].ScanSpotMetersetWeights = new_weights
-        new_dicom_data.IonBeamSequence[0].IonControlPointSequence[i].CumulativeMetersetWeight = new_cumulative_weight
+            original_cumulative_weight = ion_control_point.CumulativeMetersetWeight
+            total_original_cumulative_weight += original_cumulative_weight  # Add to total original cumulative weight
 
-        logging.info(f"Layer {i} Cumulative Weight Before: {original_cumulative_weight}")
-        logging.info(f"Layer {i} Cumulative Weight After: {new_cumulative_weight}")
+            # Use scale factor if available, otherwise use 1
+            scale_factor = scale_factors[i] if scale_factors else 1
 
-        if parsed_args.print:
-            print_comparison(i // 2 + 1, scale_factor, weights, new_weights, parsed_args.print)
+            # if a single spot is in an energy layer, it will not be stored into an array by pydicom.
+            # Therefore it is casted into an array here, in order not to break the subsequent code.
 
-    new_dicom_data.IonBeamSequence[0].IonControlPointSequence[-1].CumulativeMetersetWeight = total_new_cumulative_weight
-    for i in range(0, len(ion_control_point_sequence)):
-        new_dicom_data.IonBeamSequence[0].IonControlPointSequence[i].CumulativeDoseReferenceCoefficient = new_dicom_data.IonBeamSequence[0].IonControlPointSequence[i].CumulativeMetersetWeight/ total_new_cumulative_weight
+            if ion_control_point.NumberOfScanSpotPositions == 1:
+                weights = [ion_control_point.ScanSpotMetersetWeights]
+            else:
+                weights = ion_control_point.ScanSpotMetersetWeights
 
-    new_dicom_data.IonBeamSequence[0].FinalCumulativeMetersetWeight = total_new_cumulative_weight
-    new_dicom_data.FractionGroupSequence[0].ReferencedBeamSequence.BeamMeterset = (dicom_data.IonBeamSequence[0].FinalCumulativeMetersetWeight/original_cumulative_weight) *total_new_cumulative_weight
+            logging.info(f"length of weights {len(weights)}")
+            # Modify the weights with both scale_factor and plan_rescale_ratio
+            # print(f"weights {weights}")
+            # print(f'scale_factor {scale_factor}')
+            # print(f"plan_rescale_ratio {plan_rescale_ratio}")
+            new_weights = [w * scale_factor * plan_rescale_ratio for w in weights]
+
+
+            for w in weights:
+                logging.debug(f'wt: {w}')
+            new_cumulative_weight = original_cumulative_weight * scale_factor * plan_rescale_ratio
+            #sum(new_weights)  # Calculate the new cumulative weight
+            total_new_cumulative_weight += new_cumulative_weight  # Add to total new cumulative weight
+
+            ion_control_point.ScanSpotMetersetWeights = new_weights
+            ion_control_point.CumulativeMetersetWeight = new_cumulative_weight
+
+            logging.info(f"Layer {i} Cumulative Weight Before: {original_cumulative_weight}")
+            logging.info(f"Layer {i} Cumulative Weight After: {new_cumulative_weight}")
+
+            if parsed_args.print:
+                print_comparison(i // 2 + 1, scale_factor, weights, new_weights, parsed_args.print)
+
+            ion_control_point.CumulativeMetersetWeight = total_new_cumulative_weight
+
+        for i,ion_control_point in enumerate(ion_beam.IonControlPointSequence):  # loop over energy layers
+            ion_control_point.CumulativeDoseReferenceCoefficient = ion_control_point.CumulativeMetersetWeight / total_new_cumulative_weight
+
+        ion_beam.FinalCumulativeMetersetWeight = total_new_cumulative_weight
+        print(len(new_dicom_data.FractionGroupSequence))
+        print(len(dicom_data.IonBeamSequence))
+        new_dicom_data.FractionGroupSequence[0].ReferencedBeamSequence[j].BeamMeterset = (dicom_data.IonBeamSequence[j].FinalCumulativeMetersetWeight/original_cumulative_weight) * total_new_cumulative_weight
 
 def main(args=None):
     """Main function."""
@@ -95,13 +115,18 @@ def main(args=None):
     parser.add_argument('-o', '--output', required=False, default="output.dcm", help='Path to output DICOM file')
 
     parser.add_argument('-a', '--approve', action='store_true', default=False, help='Set plan to APPROVED')
+    parser.add_argument('-ic', '--intent_curative', action='store_true', default=False, help='Set plan intent to CURATIVE')
     parser.add_argument('-i', '--inspect', action='store_true', default=False, help='Print contents of dicom file exit')
     parser.add_argument('-tr4', '--tr4', action='store_true', default=False, help='prepare plan for TR4, this sets aproval, gantry, snout and treatment machine')
 
     parser.add_argument('-p', '--print', type=int, default=None, help='Number of random values to print for comparison')
     parser.add_argument('-g', '--gantry_angles', type=str, default=None, help='List of comma-separated gantry angles')
+    parser.add_argument('-d', '--duplicate_fields', type=int, default=None, help='Duplicate all fields in the plan n times')
     parser.add_argument('-pd', '--plan_dose', type=float, default=None, help='Nominal plan dose [Gy(RBE)]')
     parser.add_argument('-rd', '--rescale_dose', type=float, default=None, help='New rescaled dose [Gy(RBE)]')
+    parser.add_argument('-rf', '--rescale_factor', type=float, default=None, help='Multiply plan MUs by this factor')
+    parser.add_argument('-tp', '--table_position', type=str, default=None,
+                        help='New table position vertical,longitudinal,lateral [cm]. Negative values should be in quotes and leading space.')
 
     parser.add_argument('-tm', '--treatment_machine', type=str, default=None, help='Treatment Machine Name')
     parser.add_argument('-pn', '--patient_name', type=str, default=None, help='Set patient name')
@@ -119,7 +144,7 @@ def main(args=None):
         logging.basicConfig()
 
     # Check if neither weights nor rescale_dose/plan_dose are provided
-    if parsed_args.weights or parsed_args.plan_dose or parsed_args.rescale_dose:
+    if parsed_args.weights or parsed_args.plan_dose or parsed_args.rescale_dose or parsed_args.rescale_factor:
         rescale_flag = True
     else:
         rescale_flag = False  #print("Error: No scaling factor is given for rescaling the plan or spots.")
@@ -132,6 +157,7 @@ def main(args=None):
     dicom_data = pydicom.dcmread(parsed_args.inputfile)
     new_dicom_data = copy.deepcopy(dicom_data)
 
+    # todo: fix for multi field
     ion_beam_sequence = new_dicom_data.IonBeamSequence[0]
     ion_control_point_sequence = new_dicom_data.IonBeamSequence[0].IonControlPointSequence
 
@@ -145,13 +171,47 @@ def main(args=None):
             ibs.IonControlPointSequence[0].GantryAngle = gantry_angles[i]
             logging.info(f"Gantry angle field #{i+1} changed from {old_ga} to {ibs.IonControlPointSequence[0].GantryAngle}")
 
+    # rescale must be done BEFORE duplicate of fields, since it uses dicom_data which must be in sync with new_dicom_data in terms of number of fields.
+    if rescale_flag:
+        rescale(parsed_args, dicom_data, new_dicom_data)
+
+    if parsed_args.duplicate_fields:
+        n = parsed_args.duplicate_fields
+        fgs = new_dicom_data.FractionGroupSequence[0]
+        nf = new_dicom_data.FractionGroupSequence[0].NumberOfBeams
+        rbs = copy.deepcopy(fgs.ReferencedBeamSequence)
+        ibs = copy.deepcopy(new_dicom_data.IonBeamSequence)
+
+        new_rbs = [copy.deepcopy(item) for item in rbs for _ in range(n)]
+        new_ibs = [copy.deepcopy(item) for item in ibs for _ in range(n)]
+
+        new_dicom_data.FractionGroupSequence[0].ReferencedBeamSequence = new_rbs
+        new_dicom_data.FractionGroupSequence[0].NumberOfBeams = len(new_rbs)
+
+        new_dicom_data.IonBeamSequence = new_ibs
+        for i, ib in enumerate(new_dicom_data.IonBeamSequence):
+            copy_number = i % n
+            if copy_number != 0:
+                ib.BeamName += f"_copy_{copy_number}"  # append _copy
+            print(ib.BeamName)
+            ib.BeamNumber = i+1
+            new_rbs[i].ReferencedBeamNumber = i+1
+
+        logging.info(f"Duplicated {nf} fields {n} times.")
+
+
     if parsed_args.treatment_machine:
-        ion_beam_sequence.TreatmentMachineName = parsed_args.treatment_machine
-        logging.info(f"New Treatment Machine Name {ion_beam_sequence.TreatmentMachineName}")
+        for ibs in new_dicom_data.IonBeamSequence:
+            ibs.TreatmentMachineName = parsed_args.treatment_machine
+        logging.info(f"New Treatment Machine Name {new_dicom_data.IonBeamSequence[-1].TreatmentMachineName}")
 
     if parsed_args.approve:
         new_dicom_data.ApprovalStatus = "APPROVED"
         logging.info(f"New approval status {new_dicom_data.ApprovalStatus}")
+
+    if parsed_args.intent_curative:
+        new_dicom_data.PlanIntent = 'CURATIVE'
+        logging.info(f"New plan intent {new_dicom_data.PlanIntent}")
 
     if parsed_args.patient_name:
         new_dicom_data.PatientName = parsed_args.patient_name
@@ -163,23 +223,35 @@ def main(args=None):
     # TR4 specific settings
     if parsed_args.tr4:
         new_dicom_data.ApprovalStatus = "APPROVED"
-        ion_beam_sequence.TreatmentMachineName = "TR4"
-        # TODO: change for all energy layers
-        # ion_control_point_sequence.GantryAngle = "90.0"
-        # ion_control_point_sequence.SnoutPosition = "421.0"
+        for ibs in new_dicom_data.IonBeamSequence:
+            ibs.TreatmentMachineName = "TR4"
+            ibs.IonControlPointSequence[0].GantryAngle = 90.0
+            ibs.IonControlPointSequence[0].SnoutPosition = 421.0
+        logging.info(f"All gantry angles set to  {new_dicom_data.IonBeamSequence[-1].IonControlPointSequence[0].GantryAngle}")
+        logging.info(f"All snout positions set to  {new_dicom_data.IonBeamSequence[-1].IonControlPointSequence[0].SnoutPosition}")
+
+    if parsed_args.table_position:
+        tp = parsed_args.table_position.split(",")
+        if len(tp) != 3:
+            logging.error(f"Table Position expects three values, got {len(tp)}.")
+            exit()
+        for ibs in new_dicom_data.IonBeamSequence:
+            ibs.IonControlPointSequence[0].TableTopVerticalPosition = tp[0]
+            ibs.IonControlPointSequence[0].TableTopLongitudinalPosition = tp[1]
+            ibs.IonControlPointSequence[0].TableTopLateralPosition = tp[2]
+        logging.info(f"Table Vertical Position: {ibs.IonControlPointSequence[0].TableTopVerticalPosition} [cm]")
+        logging.info(f"Table Longitudinal Position: {ibs.IonControlPointSequence[0].TableTopLongitudinalPosition} [cm]")
+        logging.info(f"Table Lateral Position: {ibs.IonControlPointSequence[0].TableTopLateralPosition} [cm]")
 
 
     logging.info(f"Total Cumulative Weight Before: {dicom_data.IonBeamSequence[0].FinalCumulativeMetersetWeight}")
     logging.info(f"Total Cumulative Weight After: {new_dicom_data.IonBeamSequence[0].FinalCumulativeMetersetWeight}")
 
-    if rescale_flag:
-        rescale(parsed_args, dicom_data, new_dicom_data)
-
     new_dicom_data.save_as(parsed_args.output)
     logging.info(f"Weight rescaled plan is saved as {parsed_args.output}")
     logging.info(f"Patient name '{new_dicom_data.PatientName}'")
     logging.info(f"Approval status '{new_dicom_data.ApprovalStatus}'")
-    logging.info(f"Treatment Machine Name '{ion_beam_sequence.TreatmentMachineName}'")
+    logging.info(f"Treatment Machine Name '{new_dicom_data.IonBeamSequence[-1].TreatmentMachineName}'")
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
