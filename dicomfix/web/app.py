33fa91b9f74e7eb8f0356cf744d73e68eb020de1
Niels Bassler <bassler@clin.au.dk>
2024-09-18 23:54:21 +0200

    add deps.

--- dicomfix/web/app.py
+++ dicomfix/web/app.py
@@ -2,8 +2,13 @@ import streamlit as st
 from dicomfix.dicomutil import DicomUtil
 import io
 
+
+# Create two columns for layout
+col1, col2 = st.columns([3, 1])
+
 # Title of the web app
-st.title("DicomFix")
+with col1:
+    st.title("DicomFix")
 
 # Sidebar for file upload
 st.sidebar.title("Upload DICOM File")
@@ -66,7 +71,6 @@ if st.session_state.dicom_util:
     )
 
     # Add a widget for setting table position
-
     def set_table_position():
         # Convert the table position inputs (in cm) to mm
         vertical = st.session_state.vertical_position * 10.0
@@ -76,22 +80,18 @@ if st.session_state.dicom_util:
         update_inspect()
 
     # Table position input fields
-    st.sidebar.subheader("Set Table Position (in cm)")
+    st.sidebar.subheader("Set Table Position")
 
     ic = st.session_state.dicom_util.dicom.IonBeamSequence[0].IonControlPointSequence[0]
     # Use st.number_input for each coordinate (vertical, longitudinal, and lateral)
-    vertical = st.sidebar.number_input("Vertical Position [cm]",
-                                       value=ic.TableTopVerticalPosition * 0.1,
-                                       step=0.1, key="vertical_position")
-    longitudinal = st.sidebar.number_input("Longitudinal Position [cm]",
-                                           value=ic.TableTopLongitudinalPosition * 0.1,
-                                           step=0.1, key="longitudinal_position")
-    lateral = st.sidebar.number_input("Lateral Position [cm]", value=ic.TableTopLateralPosition * 0.1,
-                                      step=0.1, key="lateral_position")
-
-    # Add a button to apply the table position
-    if st.sidebar.button("Set Table Position"):
-        set_table_position()
+    st.sidebar.number_input("Vertical Position [cm]",
+                            value=ic.TableTopVerticalPosition * 0.1,
+                            step=0.1, key="vertical_position", on_change=set_table_position)
+    st.sidebar.number_input("Longitudinal Position [cm]",
+                            value=ic.TableTopLongitudinalPosition * 0.1,
+                            step=0.1, key="longitudinal_position", on_change=set_table_position)
+    st.sidebar.number_input("Lateral Position [cm]", value=ic.TableTopLateralPosition * 0.1,
+                            step=0.1, key="lateral_position", on_change=set_table_position)
 
     # Add a widget for setting a new dose value
     def set_new_dose():
@@ -100,34 +100,32 @@ if st.session_state.dicom_util:
         update_inspect()
 
     # Add a number input widget for the new dose
-    st.sidebar.subheader("Set New Dose (Gy[RBE])")
+    st.sidebar.subheader("Set New Dose")
 
     # Use st.number_input for the new dose
-    st.sidebar.number_input("New Dose (Gy[RBE])", value=2.0, step=0.1, key="new_dose_value")
-
-    # Add a button to apply the new dose
-    if st.sidebar.button("Set New Dose"):
-        set_new_dose()
+    dose = st.session_state.dicom_util.dicom.FractionGroupSequence[0].ReferencedBeamSequence[0].BeamDose
+    st.sidebar.number_input("New Dose (Gy[RBE])", value=dose, step=0.1, key="new_dose_value",  on_change=set_new_dose)
 
     # Modify the downloaded filename to include _DICOMFIX
     original_filename = st.session_state.uploaded_filename
     new_filename = original_filename.replace(".dcm", "_DICOMFIX.dcm")
 
-    # Download button for modified DICOM file
-    modified_dicom_buffer = io.BytesIO()
-    st.session_state.dicom_util.save(modified_dicom_buffer)
+    # Display DICOM inspection results on the left
+    with col1:
+        # Display DICOM inspection results at the end after all updates
+        st.text(st.session_state.dicom_info)
 
-    st.sidebar.download_button(
-        label="Download Modified DICOM",
-        data=modified_dicom_buffer.getvalue(),
-        file_name=new_filename,  # Updated filename
-        mime="application/octet-stream"
-    )
+    # Download button for modified DICOM file in the upper-right (col2)
+    with col2:
+        # Download button for modified DICOM file
+        modified_dicom_buffer = io.BytesIO()
+        st.session_state.dicom_util.save(modified_dicom_buffer)
 
-
-# Display DICOM inspection results at the end after all updates
-if st.session_state.dicom_util:
-    # st.text_area("DICOM Inspection", value=st.session_state.dicom_info, height=400)
-    st.text(st.session_state.dicom_info)
+        st.download_button(
+            label="Download Modified DICOM",
+            data=modified_dicom_buffer.getvalue(),
+            file_name=new_filename,  # Updated filename
+            mime="application/octet-stream"
+        )
 else:
     st.write("Please upload a DICOM file using the left sidebar.")
