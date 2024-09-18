6eddbe2995c89a61d2ab918e39c0f4099f1eaa70
Niels Bassler <bassler@clin.au.dk>
2024-09-18 21:10:31 +0200

    drop python < 3.8

--- dicomfix/__version__.py
+++ dicomfix/__version__.py
@@ -1,7 +1,3 @@
-try:
-    from importlib.metadata import version as get_version
-except ImportError:
-    # For older Python versions (pre 3.8)
-    from importlib_metadata import version as get_version
+from importlib.metadata import version as get_version
 
 __version__ = get_version("dicomfix")
