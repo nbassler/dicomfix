e6add9e27742892ae34facd43b48d916fa25abd0
Niels Bassler <7140398+nbassler@users.noreply.github.com>
2025-01-15 14:12:12 +0100

    25 remove rs tag from rs (#26)
    * fix RS and snout sequence
    
    * fix count

--- dicomfix/dicomutil.py
+++ dicomfix/dicomutil.py
@@ -570,6 +570,15 @@ class DicomUtil:
 
             # TODO, we need a helper function which only sets if attribute is missing
 
+            # Loop over snout sequence and set SnoutID to "S1"
+            for ss in ib.SnoutSequence:
+                ss.SnoutID = "S1"
+
+            # remove any range shifter sequence
+            if hasattr(ib, "RangeShifterSequence"):
+                del ib.RangeShifterSequence
+                ib.NumberOfRangeShifters = 0
+
             # Check if table position are missing. Attributes may be there, but set to None
 
             if ib.IonControlPointSequence[0].TableTopVerticalPosition is None:
